04e6e4bb4dbdc382e8b0643eab82ec9e22870394
geek developer <verma.akash045@gmail.com>
2021-03-29 09:08:53 +0530

    update

--- src/containers/TheLayout.js
+++ src/containers/TheLayout.js
@@ -15,35 +15,25 @@ const grantPermission = () => {
   const requestedRoles = localStorage.getItem("role");
   if (requestedRoles === "user") {
     return "user";
-  } else if (requestedRoles === "ROLE_SUPER_ADMIN") {
-    return "ROLE_SUPER_ADMIN";
-  } else if (requestedRoles === "ROLE_COMPANY_ADMIN") {
-    return "ROLE_COMPANY_ADMIN";
-  }
+  } else if (requestedRoles === "admin") {
+    return "admin";
+  } 
   else {
     return false;
   }
-  // console.log("requestedRoles =>", requestedRoles, permittedRoles);
-  // console.log("local  ->", localStorage);
-  // if (requestedRoles === permittedRoles) {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
 };
 const grantPermissionAdmin = () => {
   const requestedRoles = localStorage.getItem("role");
-  const permittedRoles = "ROLE_SUPER_ADMIN";
+  const permittedRoles = "admin";
   if (requestedRoles === permittedRoles) {
     return true;
   } else {
     return false;
   }
 };
-let getpermission = grantPermission()
 const TheLayout = (props) => {
   const { layout: Layout, component: Component, ...rest } = props;
-  let perm = grantPermissionAdmin();
+
   return (
     <>
       {localStorage.getItem("role") === "user" ? (
@@ -54,11 +44,9 @@ const TheLayout = (props) => {
             <div className="c-body">
               <TheContent />
             </div>
-            {/* <TheFooter /> */}
           </div>
         </div>
-      ) : localStorage.getItem("role") == "ROLE_SUPER_ADMIN" 
-       || localStorage.getItem("role") == "ROLE_COMPANY_ADMIN" || localStorage.getItem("role") == "SUPER_ADMIN_AS_COMPANY_ADMIN" || localStorage.getItem("role") == "ROLE_ADMIN" ? (
+      ) : localStorage.getItem("role") == "admin" ? (
         <div className="c-app c-default-layout">
           <TheSidebar />
           <div className="c-wrapper">
@@ -66,7 +54,6 @@ const TheLayout = (props) => {
             <div className="c-body">
               <TheContent />
             </div>
-            {/* <TheFooter /> */}
           </div>
         </div>
       ) : (
@@ -80,16 +67,6 @@ const TheLayout = (props) => {
             />
           )}
     </>
-    // <div className="c-app c-default-layout">
-    //   <TheUserSidebar />
-    //   <div className="c-wrapper">
-    //     <TheUserHeader />
-    //     <div className="c-body">
-    //       <UserPanelDashboard />
-    //     </div>
-    //     <TheFooter />
-    //   </div>
-    // </div>
   );
 };
 
